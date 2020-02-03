5ce20608ac008ba6437f37846b671d8815df1f88
hasyu <60592658+hasyu@users.noreply.github.com>
2020-02-03 14:23:51 +0900

    Initial commit.
    練習です。

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("baz").addEventListener("click", function(){
+  alert('ナイスクリック 1回目');
+  alert('ナイスクリック　2回目');
+});
+document.getElementById("saa").addEventListener("click", function(){
+  alert('ナイスクリック 3回目');
+  alert('ナイスクリック　4回目');
+});
